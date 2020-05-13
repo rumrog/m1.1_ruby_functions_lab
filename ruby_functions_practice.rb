c54d811afbdfaaacbe7be491b48266567c9cac50
Rumen Roger <rumenroger@gmail.com>
2020-05-13 08:25:12 +0100

    tiding up

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,38 +1,38 @@
-# def return_five()
-#     return 5
-# end
+def return_five()
+    return 5
+end
 
-# def return_10()
-#     return 10
-# end
+def return_10()
+    return 10
+end
 
-# def add(number_1, number_2)
-#     return number_1 + number_2
-# end
+def add(number_1, number_2)
+    return number_1 + number_2
+end
 
-# def subtract(number_1, number_2)
-#     return number_1 - number_2
-# end
+def subtract(number_1, number_2)
+    return number_1 - number_2
+end
 
-# def multiply(number_1, number_2)
-#     return number_1 * number_2
-# end
+def multiply(number_1, number_2)
+    return number_1 * number_2
+end
 
-# def divide(number_1, number_2)
-#     return number_1 / number_2
-# end
+def divide(number_1, number_2)
+    return number_1 / number_2
+end
 
-# def length_of_string(string)
-#     return string.length
-# end
+def length_of_string(string)
+    return string.length
+end
 
-# def join_string(string_1, string_2)
-#     return string_1 + string_2
-# end
+def join_string(string_1, string_2)
+    return string_1 + string_2
+end
 
-# def add_string_as_number(string_1, string_2)
-#     return string_1.to_i + string_2.to_i
-# end
+def add_string_as_number(string_1, string_2)
+    return string_1.to_i + string_2.to_i
+end
 
 def number_to_full_month_name(month_number)
     case month_number
